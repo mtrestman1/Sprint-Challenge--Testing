bfeddb606c89be8c072d08d601f74c0fd46b7ce9
Max Trestman <mtrestman1@Maxs-MacBook-Pro.local>
2019-04-12 12:06:13 -0400

    wrote get endpoing and all tests are passing now (woohoo)

--- games/games-router.js
+++ games/games-router.js
@@ -5,6 +5,14 @@ const router = express.Router()
 const db = require('../data/dbConfig');
 
 
-
+router.get('/', (req, res) => {
+    db('games')
+    .then(games => {
+        res.status(200).json(games)
+    })
+    .catch(error => {
+        res.status(500).json(error)
+    })
+})
 
 module.exports = router;
