02696458f50fcd4712c481fbd4a9d9b92f386eda
Max Trestman <mtrestman1@Maxs-MacBook-Pro.local>
2019-04-12 12:03:35 -0400

    Wrote tests for GET, all failing (yay- in good way)

--- games/games-router.spec.js
+++ games/games-router.spec.js
@@ -1,3 +1,32 @@
 const request = require('supertest');
 const server = require('../server');
 
+describe('server', () => {
+    describe('GET', () => {
+
+        it('should return 200/OK', () => {
+            return request(server)
+            .get('/api/games')
+            .then(response => {
+                expect(response.status).toBe(200)
+            })
+        })
+
+        it('should return JSON', () => {
+            return request(server)
+            .get('/api/games')
+            .then(response => {
+                expect(response.type).toBe('application/json')
+            })
+        })
+
+        it('should return an array', () => {
+            return request(server)
+            .get('/api/games')
+            .then(response => {
+                expect(response.body).toEqual([])
+            })
+        })
+
+    })
+})
