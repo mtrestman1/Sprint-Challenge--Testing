a9cca09ccd9d154c61d23f0731fea0321af35638
Max Trestman <mtrestman1@Maxs-MacBook-Pro.local>
2019-04-12 12:28:28 -0400

    Added POST and all tests passed, will try to break now

--- games/games-router.js
+++ games/games-router.js
@@ -15,4 +15,25 @@ router.get('/', (req, res) => {
     })
 })
 
+
+router.post('/', (req, res) => {
+    const { title, genre, releaseYear } = req.body;
+    if(!title || !genre || !releaseYear) {
+        return res.status(422).json({ message: 'Please provide Title, Genre, & Release Year' })
+    } else {
+        return db('games').insert(req.body)
+        .then(ids => {
+            const id = ids[0]
+            db('games')
+            .where({id})
+            .then(games => {
+                res.status(201).json(games)
+            })
+        })
+        .catch(error => {
+            res.status(500).json({ message: `There was an error adding to the DB ${error}`})
+        })
+    }
+})
+
 module.exports = router;
